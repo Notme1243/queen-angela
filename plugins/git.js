966cec3abf9d656a2a0c0c3783ba0015a01d5849
Notme1243 <92315642+Notme1243@users.noreply.github.com>
2022-03-21 06:48:37 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -13,19 +13,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
  
 ❏©drips ®zimbots☑✔
 
-*🌟𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 𝐍𝐔𝐌𝐁𝐄𝐑🎊🎊 : wa.me/27634090203?text=Hii%20DripsMemes%20Save%20for%20save.%20*
+*🌟𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 𝐍𝐔𝐌𝐁𝐄𝐑🎊🎊 : wa.me/8960570658?text=Hii%20DripsMemes%20Save%20for%20save.%20*
 
  *🌟𝐓𝐎 𝐂𝐇𝐄𝐂𝐊 𝐔𝐏𝐃𝐀𝐓𝐄𝐒⏳⌛ .update   To update Bot .update now*
 
- *🌟𝐁𝐎𝐓 𝐌𝐀𝐊𝐈𝐍𝐆 𝐕𝐈𝐃𝐄𝐎🎥📷 : https://youtu.be/QU9ffSMTLoY 𝗦𝗨𝗕𝗦𝗖𝗥𝗜𝗕𝗘*
-     
- *Instagram id: https://www.instagram.com/zim_hot_chicks_official?utm_medium=copy_link*
 
- *🌟𝐃𝐑𝐈𝐏𝐒 𝐎𝐅𝐂📎🔗: website coming soon*
 
- *🌟𝐆𝐈𝐓𝐇𝐔𝐁 𝐋𝐈𝐍𝐊📎🔗 : https://github.com/zim-bot/queen-angela*
 
- *🌟QUEEN ANGELLA 𝐂𝐑𝐄𝐀𝐓𝐄𝐃 𝐁𝐘  𝐃𝐑𝐈𝐏𝐒 𝐌𝐄𝐌𝐄𝐒 𝐒𝐔𝐁𝐒𝐂𝐑𝐈𝐁𝐄 𝐓𝐎 𝐌𝐘 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐃𝐑𝐈𝐏𝐒 𝐎𝐅𝐂 𝐅𝐎𝐑 𝐔𝐏𝐃𝐀𝐓𝐄𝐒*
+
+ *🌟 Shashikant 𝐎𝐅𝐂📎🔗: website coming soon*
+
+ *🌟𝐆𝐈𝐓𝐇𝐔𝐁 𝐋𝐈𝐍𝐊📎🔗 : https://github.com/DarkCraft/queen-angela*
+
+
 
  ❏❏©drips ®zimbots✔
 `}) 
